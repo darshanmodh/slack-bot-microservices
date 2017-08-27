8fe2e8edac731a1883c7590319c2578ba8ea7018
Darshan Modh <darshanmodh@gmail.com>
2017-08-26 18:16:37 -0700

    Created and integrated slack-bot

--- bin/run.js
+++ bin/run.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const service = require('../server/service');
+const slackClient = require('../server/slackClient');
 const http = require('http');
-
 const server = http.createServer(service);
-server.listen(3000);
+
+const slackToken = 'xoxb-233342902887-UDO4K6jTmqdXgHBRzmM2DP35';
+const logLevel = 'verbose';
+
+const rtm = slackClient.init(slackToken, logLevel);
+rtm.start();
+
+
+slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
 
 server.on('listening', function() {
     console.log(`Iris is listening on ${server.address().port} in ${service.get('env')} mode.`);
